4e236a0fff955111b5e6c9cfcbf249cfd7e4b45b
Duale <dualemohamed1@hotmail.com>
2019-09-23 13:08:33 +0200

    1

---
+++ obligOppgave1.java
@@ -0,0 +1,38 @@
+package oblig1;
+
+import java.text.DecimalFormat;
+
+class sirkel {
+    // Diameter metode
+    public static double beregnDiameter (double radius) {
+        double diameter = 2 * radius;
+        return diameter;
+    }
+    // Omkrets metode
+    public static double beregnOmkrets (double radius) {
+        double omkrets = 2 * Math.PI * radius;
+        return omkrets;
+    }
+    // Areal metode
+    public static double beregnAreal (double radius) {
+        double areal = Math.PI * Math.pow(radius, 2);
+        return areal;
+    }
+}
+
+public class obligOppgave1 {
+    public static void main(String[] args) {
+        sirkel nySirkel = new sirkel();
+        double radius = 23.5;
+
+        double diameter = nySirkel.beregnDiameter(radius);
+        double omkrets = nySirkel.beregnOmkrets(radius);
+        double areal = nySirkel.beregnAreal(radius);
+
+        // Utskrift
+        DecimalFormat df2 = new DecimalFormat(".##");
+        System.out.println("Gitt radiusen: " + radius + ". Blir diameteren: " + df2.format(diameter));
+        System.out.println("Gitt radiusen: " + radius + ". Blir omrkretsen: " + df2.format(omkrets));
+        System.out.println("Gitt radiusen: " + radius + ". Blir arealet: " + df2.format(areal));
+    }
+}
---
+++ obligOppgave2.java
@@ -0,0 +1,57 @@
+package oblig1;
+
+import static javax.swing.JOptionPane.*;
+
+class Vare {
+    public String navn;
+    public int antall;
+    public double pris;
+
+    // Konstruktør
+    public Vare (String navn, int antall, double pris) {
+        this.navn = navn;
+        if (antall > 0) {
+            this.antall = antall;
+        }
+        if (pris > 0) {
+            this.pris = pris;
+        }
+    }
+    //  GET/SET for navn
+    public String getNavn() {
+        return navn;
+    }
+    public void setNavn(String navn) {
+        this.navn = navn;
+    }
+    // GET/SET for antall
+    public int getAntall() {
+        return antall;
+    }
+    public void setAntall(int antall) {
+        if (antall > 0) {
+            this.antall = antall;
+        }
+    }
+    // GET/SET for pris
+    public double getPris() {
+        return pris;
+    }
+    public void setPris(double pris) {
+        if (pris > 0) {
+            this.pris = pris;
+        }
+    }
+    // totalpris beregnings metode
+    public double totalPris () {
+        double totalPris = pris * antall;
+        return totalPris;
+    }
+}
+
+public class obligOppgave2 {
+    public static void main(String [] args) {
+        // Vare 1
+
+    }
+}
